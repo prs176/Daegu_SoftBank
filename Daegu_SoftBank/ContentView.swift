0e3f81d140db163753b120accac129d76219bb31
prs176 <prsx176@gmail.com>
2021-10-06 09:24:48 +0900

    [CREATE] project

---
+++ Daegu_SoftBank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daegu_SoftBank
+//
+//  Created by 박세은 on 2021/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
