0ad2096760d8246a29d4d7d28190616a6a708167
prs176 <prsx176@gmail.com>
2021-10-14 23:49:48 +0900

    [ADD] response model

--- Daegu_SoftBank/Source/Model/Account.swift
+++ Daegu_SoftBank/Source/Model/Account.swift
@@ -2,39 +2,16 @@
 //  Account.swift
 //  Daegu_SoftBank
 //
-//  Created by 박세은 on 2021/10/09.
+//  Created by 박세은 on 2021/10/14.
 //
 
 import Foundation
 
-class Account: Codable, Hashable {
+class Account: Codable {
     var idx: Int = 0
-    var accountNum: String = ""
-    var bank: String = ""
-    var name: String = ""
-    var balance: Int = 0
-    
-    static func == (lhs: Account, rhs: Account) -> Bool {
-        return lhs.idx == rhs.idx
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(idx)
-    }
-    
-    internal init(idx: Int = 0, accountNum: String = "", bank: String = "", name: String = "", balance: Int = 0) {
-        self.idx = idx
-        self.accountNum = accountNum
-        self.bank = bank
-        self.name = name
-        self.balance = balance
-    }
-    
-    init() {
-        idx = 0
-        accountNum = ""
-        bank = ""
-        name = ""
-        balance = 0
-    }
+    var account: String
+    var password: String
+    var name: String
+    var money: Int = 0
+    var userId: String
 }
---
+++ Daegu_SoftBank/Source/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Daegu_SoftBank
+//
+//  Created by 박세은 on 2021/10/14.
+//
+
+import Foundation
+
+class User: Codable {
+    var id: String
+    var nick: String
+    var phone: String
+    var birth: String
+    var account: [Account]
+}
