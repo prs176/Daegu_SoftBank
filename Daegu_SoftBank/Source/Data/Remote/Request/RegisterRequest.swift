980e47e43d94b7af3ef3669b3f57e6310f6a409e
prs176 <prsx176@gmail.com>
2021-10-14 13:45:59 +0900

    [ADD] user api request

---
+++ Daegu_SoftBank/Source/Data/Remote/Request/RegisterRequest.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterRequest.swift
+//  Daegu_SoftBank
+//
+//  Created by 박세은 on 2021/10/14.
+//
+
+import Foundation
+
+class RegisterRequest: Codable {
+    var id: String
+    var pw: String
+    var nick: String
+    var phone: String
+    var birth: String
+    
+    internal init(id: String, pw: String, nick: String, phone: String, birth: String) {
+        self.id = id
+        self.pw = pw
+        self.nick = nick
+        self.phone = phone
+        self.birth = birth
+    }
+}
