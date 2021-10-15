cd544851c3426038972b423156c06c6087672267
prs176 <prsx176@gmail.com>
2021-10-15 13:34:56 +0900

    [MODIFY] type

--- Daegu_SoftBank/Source/Data/Remote/Remote/UserRemote.swift
+++ Daegu_SoftBank/Source/Data/Remote/Remote/UserRemote.swift
@@ -10,20 +10,22 @@ import Combine
 class UserRemote: BaseRemote<UserAPI> {
     func postRegister(_ request: RegisterRequest) -> AnyPublisher<String, Error> {
         return self.request(.postRegister(request))
-            .map(RegisterData.self, using: decoder)
-            .map { $0.token }
+            .map(Response<RegisterData>.self, using: decoder)
+            .map { $0.data.token }
             .eraseToAnyPublisher()
     }
     
     func postLogin(_ request: LoginRequest) -> AnyPublisher<LoginRequest, Error> {
         return self.request(.postLogin(request))
-            .map(LoginRequest.self, using: decoder)
+            .map(Response<LoginRequest>.self, using: decoder)
+            .map { $0.data }
             .eraseToAnyPublisher()
     }
     
     func getUser() -> AnyPublisher<User, Error> {
         return self.request(.getUser)
-            .map(User.self, using: decoder)
+            .map(Response<User>.self, using: decoder)
+            .map { $0.data }
             .eraseToAnyPublisher()
     }
 }
